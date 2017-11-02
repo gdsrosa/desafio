32d8ee53533ab4a56c22cca910655a16ca59fe7a
gdsrosa <gabrieldsrosa@gmail.com>
2017-11-02 12:49:22 -0200

    Edit style

--- battle-game/src/components/attack.js
+++ battle-game/src/components/attack.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Attack = ({attack}) => (
-  <button onClick={attack}  className='btn btn-secondary attack'>Attack</button>  
+  <button onClick={attack}  className='btn btn-danger attack'>Attack</button>  
 )
 
 export default Attack
