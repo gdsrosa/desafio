a40a3c86e30dba41e3a3bd306470c75abe568caf
gdsrosa <gabrieldsrosa@gmail.com>
2017-11-03 18:55:30 -0200

    Final adjustments and testing

--- battle-game/src/App.js
+++ battle-game/src/App.js
@@ -22,6 +22,48 @@ class App extends Component {
   }
 
 
+  restoreStamina () {
+    'use strict'
+    return () => {
+      this.setState({
+        playerTwo: {
+          stamina: 100,
+          name: this.state.playerTwo.name
+        },
+        playerOne: {
+          stamina: 100,
+          name: this.state.playerOne.name
+        }
+      })
+    }
+  }
+
+  handleAttackPlayerOne () {
+    'use strict'
+    return () => {
+      if (this.state.playerTwo.stamina === 0) { return alert('John Doe Ganhou! Pressione Restart...') }
+      this.setState({
+        playerTwo: {
+          stamina: this.state.playerTwo.stamina - 20,
+          name: this.state.playerTwo.name
+        }
+      })
+    }
+  }
+
+  handleAttackPlayerTwo () {
+    'use strict'
+    return () => {
+      if (this.state.playerOne.stamina === 0) { return alert('Jane Doe Ganhou! Pressione Restart...') }
+      this.setState({
+        playerOne: {
+          stamina: this.state.playerOne.stamina - 20,
+          name: this.state.playerOne.name
+        }
+      })
+    }
+  }
+
   render() {
     return (
       <div className="container-fluid">
@@ -30,46 +72,16 @@ class App extends Component {
           <div className='p1'>
             <Player playerName={this.state.playerOne.name} />
             <Stamina stamina={this.state.playerOne.stamina} />
-            <AttackButton attack={() => {
-              if (this.state.playerTwo.stamina === 0) { return alert('John Doe Ganhou!')} 
-                this.setState({ 
-                  playerTwo: { 
-                    stamina: this.state.playerTwo.stamina - 20,
-                    name: this.state.playerTwo.name
-                  } 
-                })
-              }} 
-            />
+            <AttackButton attack={this.handleAttackPlayerOne()} />
           </div>
           <div className='p2'>
             <Player playerName={this.state.playerTwo.name} />
             <Stamina stamina={this.state.playerTwo.stamina} />
-            <AttackButton attack={() => {
-              if (this.state.playerOne.stamina === 0) { return alert('Jane Doe Ganhou!') }
-              this.setState({
-                playerOne: {
-                  stamina: this.state.playerOne.stamina - 20,
-                  name: this.state.playerOne.name
-                }
-              })
-            }}
-            />
+            <AttackButton attack={this.handleAttackPlayerTwo()} />
           </div>
         </div>
         <div className='restart'>
-          <Restart restart={() => {
-            this.setState({ 
-              playerTwo: { 
-                stamina: 100,
-                name: this.state.playerTwo.name
-              }, 
-              playerOne: {
-                stamina: 100,
-                name: this.state.playerOne.name
-              }  
-            })
-            }}
-          />
+          <Restart restart={this.restoreStamina()} />
         </div>
       </div> 
     )
--- battle-game/src/App.test.js
+++ battle-game/src/App.test.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow, mount, render } from 'enzyme';
 import App from './App';
+import Title from './components/title'
+import AttackButton from './components/attack'
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });
+
+it('render the title', () => {
+  <Title />
+})
+
